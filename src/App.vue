9af23ad94599d75675c61dbdc1f08263f9193bbf
jinwoo <rudwlsdn@naver.com>
2025-10-02 17:38:24 +0900

    update

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<script setup lang="ts">
+	import { RouterView } from 'vue-router';
+	import Header from '@/components/header/Header.vue';
+</script>
+
+
+
+
+<template>
+	<div id="layout">
+		<Header />
+
+		<div id="page-wrapper">
+			<RouterView />
+		</div>
+	</div>
+</template>
+
+
+
+
+<style scoped>
+	#layout {
+		width: 100%;
+		height: 100%;
+		display: flex;
+		flex-direction: column;
+		
+		
+		& #page-wrapper {
+			position: relative;
+			width: 100%;
+			flex: 1;
+		}
+	}
+
+
+
+	
+	/* SCROLL STYLE */
+	::-webkit-scrollbar {
+		width: 1.2rem;
+		height: 1.2rem;
+		background-color: var(--color-scroll-track);
+	}
+	::-webkit-scrollbar-thumb {
+		border-radius: 9999px;
+		border-left: 0.2rem solid var(--color-scroll-track);
+		border-right: 0.2rem solid var(--color-scroll-track);
+		background-color: var(--color-scroll);
+	}
+	::-webkit-scrollbar-thumb:hover {
+		background-color: var(--color-scroll-hover);
+	}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+
+</script>
+
+
+
+
+<template>
+    <main id="home-wrapper">
+        <section id="banner">
+            
+        </section>
+    </main>
+</template>
+
+
+
+
+<style scoped>
+    #home-wrapper {
+        margin: 0 auto;
+        max-width: 1600px;
+        width: 100%;
+        height: 100%;
+        padding: 1vw;
+        display: grid;
+        grid-template-columns: repeat(12, 1fr);
+        column-gap: 1vw;
+
+
+        & #banner {
+            grid-column: span 12;
+            height: 50vw;
+        }
+    }
+</style>
